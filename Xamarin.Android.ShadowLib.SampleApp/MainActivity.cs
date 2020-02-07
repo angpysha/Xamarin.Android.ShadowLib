5fe26e83abb0f4556f69fbc5245ad36208d94b29
Andrii Petrovskyi <andrew.petrowski@outlook.com>
2020-02-07 22:57:41 +0200

    Add project files.

---
+++ Xamarin.Android.ShadowLib.SampleApp/MainActivity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using IO.Github.Inflationx.Viewpump;
+using PL.Looksoft.Shadowlib;
+
+namespace Xamarin.Android.ShadowLib.SampleApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void AttachBaseContext(Context @base)
+        {
+            base.AttachBaseContext(ViewPumpContextWrapper.Wrap(@base));
+        }
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var builder = ViewPump.InvokeBuilder();
+            builder.AddInterceptor(new ShadowsInterceptor(this,new ShadowLogger()));
+            ViewPump.Init(builder.Build());
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            global::Android.Support.V7.Widget.Toolbar toolbar = FindViewById<global::Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            var vv = new View(this);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (global::Android.Views.View.IOnClickListener)null).Show();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] global::Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        class ShadowLogger : Java.Lang.Object,IShadowsLogger
+        {
+            public void Log(string p0)
+            {
+                
+            }
+        }
+	}
+
+
+}
+
